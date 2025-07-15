d179703efca6403f1debb426690f14c9a987f04f
linvisn <118055226+linvisn@users.noreply.github.com>
2025-07-15 19:40:58 +0300

    Update README and implement word processing in Practice component

--- pages/Practice.vue
+++ pages/Practice.vue
@@ -1,14 +1,17 @@
 <script setup>
-import words from '~/assets/data/words.json'
+import words from '~/assets/data/words_processed.json'
 
 const isPreparation = ref(true)
 const chosenLetters = ref([])
 const array = ref([])
-const currentWord = ref('')
+const currentWord = ref({})
 const answer = ref('')
 const pointsAmount = ref(0)
 const maxPoints = ref(0)
-const overlay = ref(false)
+const showResults = ref(false)
+const mistakes = ref([])
+const showMistakes = ref(false)
+const chipColor = ref('')
 
 const extractLetters = () => {
     return words.map((word) => word.letter)
@@ -16,6 +19,8 @@ const extractLetters = () => {
 const startPractice = () => {
     isPreparation.value = false
     pointsAmount.value = 0
+    mistakes.value = []
+    chipColor.value = ''
 
     array.value = words.filter(item => chosenLetters.value.includes(item.letter)).flatMap(item => item.words)
     let currentIndex = array.value.length
@@ -30,7 +35,16 @@ const startPractice = () => {
     currentWord.value = array.value[0]
 }
 const checkWord = () => {
-    if (answer.value === currentWord.value) pointsAmount.value++
+    let isCorrect = Array.isArray(currentWord.value.word) ? currentWord.value.word.includes(answer.value) : currentWord.value.word === answer.value
+
+    if (isCorrect) {
+        pointsAmount.value++
+        chipColor.value = 'green'
+    }
+    else {
+        mistakes.value.push({ answer: answer.value, word: currentWord.value.word })
+        chipColor.value = 'red'
+    }
 
     if (array.value.length > 1) {
         array.value.shift()
@@ -38,7 +52,7 @@ const checkWord = () => {
     }
     else {
         isPreparation.value = true
-        overlay.value = !overlay.value
+        showResults.value = !showResults.value
     }
 
     answer.value = ''
@@ -48,31 +62,49 @@ const checkWord = () => {
 <template>
     <div v-if="isPreparation">
         <div class="text-h5 font-weight-bold">Оберіть нижченаведені параметри перед тим, як розпочати</div>
-        <v-select class="mt-6 mb-10" v-model="chosenLetters" label="літери, з яких мають починатися слова"
-            :items="extractLetters()" multiple clearable />
+        <v-select class="mt-6 mb-10" v-model="chosenLetters" label="літери, з яких мають починатися слова" :items="extractLetters()" multiple clearable />
 
         <div class="d-flex align-center flex-wrap ga-4">
-            <v-btn class="bg-deep-orange-accent-4" size="large" variant="tonal" elevation="8"
-                :disabled="chosenLetters.length < 1" @click="startPractice()">Розпочати</v-btn>
+            <v-btn class="bg-deep-orange-accent-4" size="large" variant="tonal" elevation="8" :disabled="chosenLetters.length < 1" @click="startPractice()">Розпочати</v-btn>
             <v-btn @click="chosenLetters = extractLetters()">Обрати всі літери</v-btn>
             <NuxtLink to="/">
                 <v-btn variant="text">Повернутися до головної сторінки</v-btn>
             </NuxtLink>
         </div>
 
-        <v-overlay v-model="overlay" class="d-flex align-center justify-center">
-            <v-card title="Результат">
+        <v-overlay v-model="showResults" class="d-flex align-center justify-center">
+            <v-card class="d-flex flex-column align-center" title="Результат">
                 <v-card-text>Ви правильно визначили наголоси в {{ pointsAmount }} з {{ maxPoints }} слів!</v-card-text>
+                <v-btn v-if="mistakes.length > 0" class="mb-3" variant="tonal" @click="showMistakes = true; showResults = false">Переглянути помилки</v-btn>
+            </v-card>
+        </v-overlay>
+
+        <v-overlay v-model="showMistakes" class="d-flex align-center justify-center">
+            <v-card title="Помилки, яких ви припустилися">
+                <v-table :striped="'odd'" fixed-header hover>
+                    <thead>
+                        <tr>
+                            <th>Ваша відповідь</th>
+                            <th>Правильна відповідь</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(mistake, mistakeIndex) in mistakes" :key="mistakeIndex">
+                            <td>{{ mistake.answer }}</td>
+                            <td>{{ Array.isArray(mistake.word) ? mistake.word.join('/') : mistake.word }}</td>
+                        </tr>
+                    </tbody>
+                </v-table>
             </v-card>
         </v-overlay>
     </div>
 
     <div v-else>
-        <div class="text-h2 text-lowercase text-break" :style="{ 'user-select': 'none' }">{{ currentWord }}</div>
+        <div class="text-h2 text-break">{{ currentWord.display }}</div>
 
         <div class="d-flex flex-wrap ga-2 my-6">
             <v-chip>Слів залишилось: {{ array.length }}</v-chip>
-            <v-chip>{{ pointsAmount }} з {{ maxPoints }} правильних відповідей</v-chip>
+            <v-chip :color="chipColor">{{ pointsAmount }} з {{ maxPoints }} правильних відповідей</v-chip>
         </div>
 
         <v-text-field v-model="answer" label="Запиши слово маленькими літерами, а наголошені голосні - великими" />
@@ -80,8 +112,7 @@ const checkWord = () => {
         <div class="d-flex flex-wrap ga-4">
             <v-btn v-if="array.length > 1" variant="tonal" @click="checkWord()">Перевірити</v-btn>
             <v-btn v-else class="bg-green" size="large" @click="checkWord()">Закінчити</v-btn>
-            <v-btn v-if="array.length > 1" variant="text" @click="isPreparation = true">Повернутися до
-                налаштувань</v-btn>
+            <v-btn v-if="array.length > 1" variant="text" @click="isPreparation = true">Повернутися до налаштувань</v-btn>
         </div>
     </div>
 </template>
