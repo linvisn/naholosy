3963074a5ae2d9d96277ffd81dfd4678420ea886
linvisn <118055226+linvisn@users.noreply.github.com>
2025-07-17 13:19:26 +0300

    Enhance SEO metadata and set HTML language attribute in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,17 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: 'naholosy',
+      htmlAttrs: {
+        lang: 'uk'
+      },
       meta: [
         { name: 'description', content: 'Тренажер наголосів з української мови для підготовки до ЗНО та НМТ' },
+        { property: 'og:title', content: 'naholosy' },
+        { property: 'og:description', content: 'Тренажер наголосів з української мови для підготовки до ЗНО та НМТ' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:url', content: 'https://naholosy.deno.dev/' },
+        { property: 'og:image', content: 'https://naholosy.deno.dev/favicon.ico' },
+        { property: 'og:locale', content: 'uk_UA' }
       ]
     }
   },
