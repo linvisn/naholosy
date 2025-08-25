b4d68209cf27fe09a6ea22335001946c8d2f6268
linvisn <118055226+linvisn@users.noreply.github.com>
2025-08-25 15:41:21 +0300

    Refactor index.vue: Update text styles and improve footer layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,8 +9,8 @@ const toggleTheme = () => {
 </script>
 
 <template>
-  <div class="mt-4 mt-sm-8 text-h2 text-center font-weight-bold" :style="{ 'line-height': 1.35 }">
-    Вітаємо в тренажері <span class="px-3 bg-blue-lighten-1">наголосів</span> для <span class="px-3 bg-teal-accent-4">ЗНО/НМТ</span>
+  <div class="mt-4 mt-sm-8 text-h3 text-md-h2 text-center font-weight-bold" :style="{ 'line-height': 1.35 }">
+    Вітаємо в тренажері <span class="px-3 bg-blue">наголосів</span> для <div><span class="px-3 bg-light-green-darken-1">ЗНО/НМТ</span></div>
   </div>
 
   <div class="d-flex flex-wrap align-center justify-center ga-6 my-10">
@@ -18,7 +18,7 @@ const toggleTheme = () => {
       <v-btn size="x-large" variant="tonal" elevation="8">Розпочати тренування</v-btn>
     </NuxtLink>
     <v-btn size="large" @click="toggleTheme()">Перемкнути тему</v-btn>
-    <a href="https://www.privat24.ua/send/hz9q7" target="_blank"><v-btn size="large">Підтримати розробку</v-btn></a>
+    <a href="https://www.privat24.ua/send/hz9q7" target="_blank" @click="saEvent('click_support')"><v-btn size="large">Підтримати розробку</v-btn></a>
   </div>
 
   <v-card class="py-1 border" variant="elevated">
@@ -32,9 +32,9 @@ const toggleTheme = () => {
     </v-table>
   </v-card>
 
-  <v-footer class="d-flex align-center justify-center flex-column mt-10 mx-auto px-10 border"
+  <v-footer class="d-flex align-center justify-center flex-column mt-10 mx-auto px-5 border"
     :style="{ width: 'fit-content' }">
-    <div class="text-overline text-justify">Бажаєте допомогти з покращенням застосунку? Перейдіть до <a
+    <div class="text-subtitle-1 text-center">Бажаєте допомогти з покращенням застосунку? Перейдіть до <a
         href="https://github.com/linvisn/naholosy">репозиторію</a>, аби дізнатися більше.</div>
     <v-divider class="my-2" thickness="1" width="200" />
     <div class="text-caption">2025 - <a href="https://github.com/linvisn/">linvisn</a></div>
